97785718943f493c6bd9b299907160e13de91dc8
Thiago Lopes <tlopesdm@gmail.com>
2025-04-16 20:59:42 -0300

    fixing getUserProfile

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,13 +1,6 @@
 import { JwtAuthGuard } from '@guards/jwt-auth.guard';
-import {
-  Controller,
-  Get,
-  NotFoundException,
-  Param,
-  Req,
-  UseGuards,
-} from '@nestjs/common';
-import { AuthRequest } from '../../common/utils/interfaces/auth-request.interface';
+import { AuthRequest } from '@interfaces/auth-request.interface';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { User } from './entities/user.entity';
 import { UserResponse } from './interfaces/user-response.interface';
 import { UsersService } from './users.service';
@@ -24,22 +17,19 @@ export class UsersController {
 
   @Get('profile')
   async getUserProfile(@Req() req: AuthRequest): Promise<UserResponse> {
-    const user = req.user;
-    if (!user) {
-      throw new NotFoundException('Current User not found');
-    }
-    return this.usersService.getUserProfile(user.sub);
+    const userId = req.user.sub;
+    return this.usersService.getUserProfile(userId);
   }
 
-  @Get(':id')
-  async findUser(@Param('id') id: string): Promise<User> {
-    try {
-      return await this.usersService.findById(id);
-    } catch (error) {
-      if (error instanceof NotFoundException) {
-        throw error;
-      }
-      throw new Error('Failed to Fetch User');
-    }
-  }
+  // @Get(':id')
+  // async findUser(@Param('id') id: string): Promise<User> {
+  //   try {
+  //     return await this.usersService.findById(id);
+  //   } catch (error) {
+  //     if (error instanceof NotFoundException) {
+  //       throw error;
+  //     }
+  //     throw new Error('Failed to Fetch User');
+  //   }
+  // }
 }
